e2b61fcbd6fa53ad8237e2a3177f8a647168bb42
jiaxinxuan <552387367@qq.com>
2017-05-23 17:50:56 +0800

    Python爬虫第一步

---
+++ src/spider/spider.py
@@ -0,0 +1,27 @@
+'''
+Created on 2017年5月23日
+
+@author: Administrator
+'''
+# import urllib.request
+# import urllib
+#  
+# url = "http://www.baidu.com"
+# data = urllib.request.urlopen(url).read()
+# data = data.decode('UTF-8')
+# urllib.parse.urlencode(data)
+# print(data)
+
+import urllib
+import urllib.request
+ 
+data={}
+data['word']='Jecvay Notes'
+ 
+url_values=urllib.parse.urlencode(data)
+url="http://www.baidu.com/s?"
+full_url=url+url_values
+ 
+data=urllib.request.urlopen(full_url).read()
+data=data.decode('UTF-8')
+print(data)
