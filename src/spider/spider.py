00ca2a74ecce5fe7ef2a8c414112d86f747f9e34
jiaxinxuan <552387367@qq.com>
2017-05-25 09:44:53 +0800

    抓取简单练习

--- src/spider/spider.py
+++ src/spider/spider.py
@@ -14,14 +14,20 @@ Created on 2017年5月23日
 
 import urllib
 import urllib.request
- 
-data={}
-data['word']='Jecvay Notes'
- 
-url_values=urllib.parse.urlencode(data)
-url="http://www.baidu.com/s?"
-full_url=url+url_values
- 
-data=urllib.request.urlopen(full_url).read()
-data=data.decode('UTF-8')
+  
+# data={}
+# data['word']='Jecvay Notes'
+#  
+# url_values=urllib.parse.urlencode(data)
+# print(url_values)
+# url="http://www.baidu.com/s?"
+# full_url=url+url_values
+# print(full_url)
+# data=urllib.request.urlopen(full_url).read()
+# data=data.decode('UTF-8')
+# print(data)
+
+url="http://www.baidu.com"
+data=urllib.request.urlopen(url).read()
+data.decode('UTF-8')
 print(data)
