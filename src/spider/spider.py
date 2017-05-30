8e919ff66edcac35ed130335d67d65ee0c5deba3
jiaxinxuan <552387367@qq.com>
2017-05-31 06:48:54 +0800

    修改

--- src/spider/spider.py
+++ src/spider/spider.py
@@ -3,6 +3,8 @@ Created on 2017年5月23日
 
 @author: Administrator
 '''
+import requests
+from bs4 import BeautifulSoup
 # import urllib.request
 # import urllib
 #  
@@ -12,22 +14,30 @@ Created on 2017年5月23日
 # urllib.parse.urlencode(data)
 # print(data)
 
-import urllib
-import urllib.request
-  
+# import urllib
+# import urllib.request
+# import sys,os
 # data={}
 # data['word']='Jecvay Notes'
-#  
+#    
 # url_values=urllib.parse.urlencode(data)
 # print(url_values)
 # url="http://www.baidu.com/s?"
 # full_url=url+url_values
-# print(full_url)
 # data=urllib.request.urlopen(full_url).read()
 # data=data.decode('UTF-8')
 # print(data)
 
-url="http://www.baidu.com"
-data=urllib.request.urlopen(url).read()
-data.decode('UTF-8')
-print(data)
+# try:
+#     fsock=open(file="F:/data/service/seo/product/file/index.html",encoding="UTF-8",mode="r",)#读取文件
+#     fsock=open(file="F:/data/service/seo/product/file/index123.html",encoding="UTF-8",mode="a+")
+#     fsock.write(data)
+# except IOError:
+#     print ("The file don't exist, Please double check!")
+#     exit()
+# fsock.flush()
+# content=fsock.read()
+# print(content)
+# print(fsock.name)
+# print(fsock.mode)
+# fsock.close()
